8b1fdc97400766ba8c9912084d3c543ef8f3102b
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-08 04:24:07 +0300

    Updated task 5

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,6 +1,3 @@
-#!/usr/bin/python3
-str = "Holberton School"
-print(str * 3)
-print()
-print(str[:10])
-print()
+#!/usr/bin/python3 
+ str = "Holberton School" 
+ print("{}\n{}".format((3 * str), str[:9]))
