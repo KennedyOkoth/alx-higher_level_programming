590834793f51d7c2e25086415a072897fde732fd
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-10 15:14:11 -0700

    Changed some files

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,4 +1,7 @@
 def islower(c):
     # Check if the character is within the ASCII range of lowercase letters
-    return ord('a') <= ord(c) <= ord('z')
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
 
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def uppercase(s):
     for char in s:
-        # Convert lowercase characters to uppercase using ASCII values
-        if ord('a') <= ord(char) <= ord('z'):
-            print(chr(ord(char) - 32), end="")
-        else:
-            print(char, end="")
-    print()  # Print a newline at the end
+        if ord(c) >= 97 and ord(c) <= 122:
+            c = chr(ord(c) - 32)
+        print("{}".format(c), end="")
+    print("")
 
-# Test the function
-uppercase("Hello, World!")  # Should print "HELLO, WORLD!"
