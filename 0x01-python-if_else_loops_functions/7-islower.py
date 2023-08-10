33db4e7c62faab6f738d4b414f10f344d6c0ad9f
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-10 08:42:29 -0700

    Made the files executable

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,7 +2,3 @@ def islower(c):
     # Check if the character is within the ASCII range of lowercase letters
     return ord('a') <= ord(c) <= ord('z')
 
-# Test the function
-print(islower('a'))  # Should print True
-print(islower('A'))  # Should print False
-print(islower('1'))  # Should print False
