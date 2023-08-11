d6c2423780bfbab583ba14687360d8699ec7bdde
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-11 20:53:47 +0300

    Updated 100-my_calculator.py

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from sys import argv
+from sys import exit
+from calculator_1 import add
+from calculator_1 import sub
+from calculator_1 import mul
+from calculator_1 import div
+
+if __name__ == "__main__":
+    length_of_argv = len(argv)
+    if length_of_argv < 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operator = argv[2]
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+    if operator == "+":
+        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(num1, num2, sub(num1, num2)))
+    elif operator == "*":
+        print("{:d} * {:d} = {:d}".format(num1, num2, mul(num1, num2)))
+    elif operator == "/":
+        print("{:d} / {:d} = {:d}".format(num1, num2, div(num1, num2)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
