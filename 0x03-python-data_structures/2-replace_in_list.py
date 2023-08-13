b2a746e0bb3caca854071b4a7c9bbd3bb5660654
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-13 20:31:23 +0300

    ompleted task 2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    for y in range(len(my_list)):
+        if y == idx:
+            my_list[y] = element
+            return my_list
+    return None
