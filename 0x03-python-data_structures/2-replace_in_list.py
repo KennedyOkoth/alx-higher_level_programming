47df233f31bd603c3672fc24db04c418f7c6690b
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-13 20:33:04 +0300

    updated file

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
@@ -6,4 +6,4 @@ def replace_in_list(my_list, idx, element):
         if y == idx:
             my_list[y] = element
             return my_list
-    return None
+    return my_list
