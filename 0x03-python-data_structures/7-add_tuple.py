9b0417f7a244d0d9f18df88a69bd1f3e05312241
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-14 19:01:16 +0300

    Updated file 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    for i in range(len(tuple_a)):
-        results = tuple(tuple_a(i) + tuple_b(i))
-        return results
+    tuple_a = tuple_a + (0, 0)
+    tuple_b = tuple_b + (0, 0)
+    new_tuple = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return new_tuple
+
+
+# if __name__ == "__main__":
+#     tuple_a = (1, 89)
+#     tuple_b = (88, 11)
+#     new_tuple = add_tuple(tuple_a, tuple_b)
+#     print(new_tuple)
+
+#     print(add_tuple(tuple_a, (1, )))
+#     print(add_tuple(tuple_a, ()))
