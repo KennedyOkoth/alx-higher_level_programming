264ad12a920269677f333562602744f29ad18adf
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-14 19:22:04 +0300

    Completed task 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    length = len(my_list)
+    if length == 0:
+        return None
+    else:
+        int_max = 0
+        for i in my_list:
+            if i > int_max:
+                int_max = i
+    return int_max
+
+
+# if __name__ == "__main__":
+#     my_list = [1, 90, 2, 13, 34, 5, -13, 3]
+#     max_value = max_integer(my_list)
+#     print("Max: {}".format(max_value))
