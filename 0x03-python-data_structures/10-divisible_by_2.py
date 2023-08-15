cb134f0890847b8cf512df394d50d37f2041504a
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-15 05:41:02 +0300

    Updated files

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,9 +5,9 @@ def divisible_by_2(my_list=[]):
     new_list = []
     for i in range(len(my_list)):
         if i % 2 == 0:
-            new_list.append(True)
+            new_list.append(1)
         else:
-            new_list.append(False)
+            new_list.append(0)
     return new_list
 
 
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,6 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    length = len(my_list)
-    if length == 0:
-        return None
     else:
         int_max = 0
         for i in my_list:
