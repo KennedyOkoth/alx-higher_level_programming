d79868a5a9da87f3a8dac08537034bc397739372
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-19 21:49:43 +0300

    Updated file 6

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    my_keys = list(a_dictionary.keys())
-    my_keys.sort()
-    sorted_dict = {i: a_dictionary[i] for i in my_keys}
-    return sorted_dict
+    sorted_keys = sorted(a_dictionary.keys())
+    for key in sorted_keys:
+        key_values = a_dictionary[key]
+        print(f"{key}: {key_values}")
 
 
 # if __name__ == "__main__":
-#     a_dictionary = {
+#      a_dictionary = {
 #         "language": "C",
-#         "Number": 89,
-#         "track": "Low level",
-#         "ids": [1, 2, 3],
-#     }
-#     print(print_sorted_dictionary(a_dictionary))
+#          "Number": 89,
+#          "track": "Low level",
+#          "ids": [1, 2, 3],
+#      }
+#      print(print_sorted_dictionary(a_dictionary))
