664fa475513c441de8c935396595182f6adadacd
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-30 18:50:00 +0300

    Updaeted well

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,25 +6,9 @@ class Square:
     """A class Square that defines a square with a size private key"""
 
     def __init__(self, size=0):
-        """Instantiation with optional size:
-
-        Args:
-            size (int, optional): size of the square
-
-        Raises:
-            TypeError: raised when the value is not of int type.
-            ValueError: raised when value is less than 0
-        """
+        """Instantiation with optional size:"""
 
         self.__size = size
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
 
     @property
     def size(self):
@@ -34,6 +18,23 @@ class Square:
             size: size of square
         """
         return self.__size
+
+    @size.setter
+    def size(self, value):
+        """A method to set size
+
+        Args:
+             value (int, optional): size of the square
+
+         Raises:
+             TypeError: raised when the value is not of int type.
+             ValueError: raised when value is less than 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
 
     def area(self):
         """A method to calculate area
