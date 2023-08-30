4799a25453185c62e2c21e3802491f4859fbb29d
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-30 17:14:57 +0300

    Completed task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A class Square that defines a square with a size private key"""
+
+
+class Square:
+    """A class Square that defines a square with a size private key"""
+
+    def __init__(self, size):
+        """Instation with size
+
+        Args:
+            size (Any type): private instance attribute
+        """
+        self._size = size
