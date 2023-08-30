7a816dad9b1378a7b2a00e23341812f90c04cf9d
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-30 17:48:25 +0300

    Completed task 2

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,7 +5,7 @@
 class Square:
     """A class Square that defines a square with a size private key"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Instation with size
 
         Args:
---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A class Square that defines a square"""
+
+
+class Square:
+    """A class Square that defines a square with a size private key"""
+
+    def __init__(self, size=0):
+        """Instantiation with optional size:
+
+        Args:
+            size (int, optional): size of the square
+
+        Raises:
+            TypeError: raised when the value is not of int type.
+            ValueError: raised when value is less than 0
+        """
+        self.__size = size
+        try:
+            if not isinstance(self.__size, int):
+                raise TypeError("size must be an integer")
+        except TypeError as e:
+            print(e)
+        try:
+            if self.__size < 0:
+                raise ValueError("size must be >= 0")
+        except ValueError as v:
+            print(v)
