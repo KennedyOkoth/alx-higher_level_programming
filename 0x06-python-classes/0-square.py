ebedc622faa4509366db1e18926aa6f218741366
KennedyOkoth <keinokoth.ok@gmail.com>
2023-08-30 12:24:16 +0300

    Updated files

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
-class Square(object):
+class Square:
     """An empty class that defines a square."""
 
     pass
+
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
