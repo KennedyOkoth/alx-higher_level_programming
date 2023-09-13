a0e9a68e633de417a5c333d9ba34d94bb3d9937a
KennedyOkoth <keinokoth.ok@gmail.com>
2023-09-14 00:39:02 +0300

    Completed task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,7 +2,7 @@
 """A function to write in a file"""
 
 
-def write_file(filename=''):
+def write_file(filename='', text=''):
     """My writing in a file function"""
     with open(filename, "w", encoding="utf-8") as f:
-        print(f.write('This is a text\n'))
+        return f.write(text)
