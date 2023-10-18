5220b520c97a0f1c8a441080987b7d73df7a5a67
KennedyOkoth <keinokoth.ok@gmail.com>
2023-10-18 15:36:22 +0300

    Updated files 3 and 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- Creates  a table called "first_table" in the current database with these requirements:
+-- - id INT
+-- - name VARCHAR(256)
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
