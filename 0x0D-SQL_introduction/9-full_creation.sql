d1382224acbb0f49c8b7a17e1d2bea997fc563e8
KennedyOkoth <keinokoth.ok@gmail.com>
2023-10-21 17:50:13 +0300

    Completed the required tasks.

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,13 @@
+-- Create a table named 'second_table' with columns 'id', 'name', and 'score' if it does not exist.
+CREATE TABLE IF NOT EXISTS `second_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+
+-- Insert data into the 'second_table' with individual rows.
+-- Each INSERT statement adds a new row with specified values.
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
