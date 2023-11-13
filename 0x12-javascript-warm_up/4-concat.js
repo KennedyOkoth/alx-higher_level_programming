ee8ee8cad674f869761525aa0371de72bac315f2
KennedyOkoth <keinokoth.ok@gmail.com>
2023-11-13 08:51:10 -0800

    Updated task 4.

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
-let myVar1 = process.argv[2];
-let myVar2 = process.argv[3];
-let myVar3 = 'undefined';
+const myVar1 = process.argv[2];
+const myVar2 = process.argv[3];
+const myVar3 = 'undefined';
 
 if (myVar1 === undefined) {
-	console.log(myVar3 + ' is ' + myVar3);
+  console.log(myVar3 + ' is ' + myVar3);
 } else if (myVar2 === undefined) {
-	console.log(myVar1 + ' is ' + myVar3);
+  console.log(myVar1 + ' is ' + myVar3);
 } else {
-	console.log(myVar1 + ' is ' + myVar2);
+  console.log(myVar1 + ' is ' + myVar2);
 }
