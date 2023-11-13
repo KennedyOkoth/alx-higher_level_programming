bccde48b6956c036bb51428baf06cca8647553c0
KennedyOkoth <keinokoth.ok@gmail.com>
2023-11-13 04:49:18 -0800

    Completed task 1.

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
 
-const myVar = 'C is fun\nPython is cool\nJavascript is amazing';
-console.log(myVar);
+const line1 = 'C is fun';
+const line2 = 'Pthon is cool';
+const line3 = 'Javasript is amazing';
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
