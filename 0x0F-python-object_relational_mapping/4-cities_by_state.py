74c6ef433e19ac076509a665452531bee32af955
KennedyOkoth <keinokoth.ok@gmail.com>
2023-11-20 01:26:15 +0300

    Add City and State models

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -14,13 +14,11 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        """SELECT cities.name FROM
-                cities INNER JOIN states ON states.id=cities.state_id
-                WHERE states.name=%s""",
-        (sys.argv[4],),
+        """SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id"""
     )
     rows = cur.fetchall()
-    tmp = list(row[0] for row in rows)
-    print(*tmp, sep=", ")
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
