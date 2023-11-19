85f95409600115a06abce3cd860c33789480c3df
KennedyOkoth <keinokoth.ok@gmail.com>
2023-11-20 00:53:52 +0300

    Refactor database connection code

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,8 +6,14 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host = "localhost", user = sys.argv[1],
-            password = sys.argv[2], db = sys.argv[3], port = 3306)
+    """connecting to database"""
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        password=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+    )
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states")
     rows = cursor.fetchall()
@@ -15,4 +21,3 @@ if __name__ == "__main__":
         print(row)
     cursor.close()
     db.close()
-
