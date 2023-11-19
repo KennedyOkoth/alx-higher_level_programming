5ee657e145e829ebe370b1eebb3b679b08dda511
KennedyOkoth <keinokoth.ok@gmail.com>
2023-11-20 01:06:22 +0300

    Fix SQL query to filter states by name starting with 'N'

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -15,7 +15,10 @@ if __name__ == "__main__":
         port=3306,
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name %N%")
+    cursor.execute(
+        """SELECT * FROM states WHERE name
+                   LIKE BINARY 'N%' ORDER BY states.id"""
+    )
     rows = cursor.fetchall()
     for row in rows:
         print(row)
