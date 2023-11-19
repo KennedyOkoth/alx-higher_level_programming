7d288a1db074c416092dc966d76ccdafee33f025
KennedyOkoth <keinokoth.ok@gmail.com>
2023-11-19 21:26:22 +0300

    Completed tsk1.

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+
+""" Connecting to hbtn_0e_0_usa database and list all states. """
+import MySQLdb
+import sys
+
+
+if __name__ = "__main__":
+    db = MySQLdb.connect(host = "localhost", user = sys.argv[1],
+            password = sys.argv[2], db = sys.argv[3], port = 3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
