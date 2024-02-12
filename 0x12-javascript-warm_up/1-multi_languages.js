6cd3c5e7a2ae57a34c0bcc01717fc9d5d262c77a
KennedyOkoth <keinokoth.ok@gmail.com>
2024-02-12 20:59:52 +0300

    Deleted all files.

--- 0x12-javascript-warm_up/1-multi_languages.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-myVar1 = "C is fun";
-myVar2 = "Python is cool";
-myVar3 = "JavaScript is amazing";
-
-console.log(myVar1);
-console.log(myVar2);
-console.log(myVar3);
--- 0x12-javascript-warm_up/3-value_argument.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const { argv } = require('node:process');
-
-if (process.argv[2] === undefined) {
-    console.log("No argument");
-} else {
-    console.log(process.argv[2])
-}
