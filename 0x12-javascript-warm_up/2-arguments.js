7ce9f4f08499b7a4fd916fded55b68eb40bd6cfd
KennedyOkoth <keinokoth.ok@gmail.com>
2024-02-12 22:12:41 +0300

    Completed task 2.

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-let num = argv.length;
-if (num === 2) {
+if (argv.length === 2) {
 	console.log('No argument');
-} else if (num === 3) {
+} else if (argv.length === 3) {
 	console.log('Argument found');
 } else {
 	console.log('Arguments found');
