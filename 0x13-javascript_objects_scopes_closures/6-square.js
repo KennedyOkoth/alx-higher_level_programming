9f8f5a0b2a344f50b052361972ae4bf582761184
KennedyOkoth <keinokoth.ok@gmail.com>
2024-02-13 23:10:33 +0300

    Completed task 7.

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node;
-const Square1 = require('./5-square.js');
+const Square1 = require("./5-square.js");
 class Square extends Square1 {
-  charPrint (c) {
-    c = c || 'X';
+  charPrint(c) {
+    c = c || "X";
     const r = c.repeat(super.size);
     for (let j = 0; j < super.size; ++j) {
       console.log(r);
