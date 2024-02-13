47ba2305ccd1b6776c85dc46abc632c16992420c
KennedyOkoth <keinokoth.ok@gmail.com>
2024-02-13 15:40:32 +0300

    Completed task 3.

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,16 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0 && w === parseInt(w) && h === parseInt(h)) {
       this.width = w;
       this.height = h;
+    }
+  }
+
+  print () {
+    const r = 'X'.repeat(this.width);
+    for (let j = 0; j < this.height; ++j) {
+      console.log(r);
     }
   }
 }
