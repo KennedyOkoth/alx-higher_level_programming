f0a56ed5633efa05851b6ae6c488dd51eb7741b2
KennedyOkoth <keinokoth.ok@gmail.com>
2024-02-13 16:21:08 +0300

    Completed task 5.

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
-  constructor (w, h, size) {
-    super (w, h);
+  constructor(w, h, size) {
+    super(w, h);
     this.size = size;
   }
 }
