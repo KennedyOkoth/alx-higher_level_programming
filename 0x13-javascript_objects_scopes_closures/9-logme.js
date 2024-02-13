8c306bdae94a6100a93d7131a70b8bfc79fca5f7
KennedyOkoth <keinokoth.ok@gmail.com>
2024-02-14 00:07:00 +0300

    Completed task 8.

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,12 +1,2 @@
 #!/usr/bin/node
 
-exports.esrever = function(list) {
-  let reversedList = list.slice();
-  
-  for (let i = 0, j = reversedList.length - 1; i < j; i++; j++) {
-    let temp = reversedList[i];
-    reversedList[i] = reversedList[j];
-    reversedList[j] = temp;
-  }
-  return reversedList;
-};
