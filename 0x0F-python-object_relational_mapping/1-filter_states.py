56fd3bc052cdec12fa757ea73d1b48bf8a4ef75e
KennedyOkoth <107689277+KennedyOkoth@users.noreply.github.com>
2024-02-16 20:40:48 +0300

    Updated task 1.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Filters stste in a database accordin to letter N at the begining."""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306,
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE LEFT(name, 1) = 'N'")
+    names = cur.fetchall()
+    for name in names:
+        print(name)
+
+    cur.close()
+    db.close()
