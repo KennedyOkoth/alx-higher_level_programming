8213b7fa77f09d332427bb19f8240ebc5672a65f
KennedyOkoth <keinokoth.ok@gmail.com>
2024-03-28 00:57:50 +0300

    Created a file

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0 && w === parseInt(w) && h === parseInt(h)) {
       this.width = w;
       this.height = h;
@@ -14,4 +14,5 @@ class Rectangle {
     }
   }
 }
+
 module.exports = Rectangle;
